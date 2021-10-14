649d6cbd6949371bd039feb8731627d8b4a0696f
Алексей Сергиенко <lehazyo@gmail.com>
2021-10-14 21:42:17 +0300

    initialRelief changed

--- src/components/aquarium/aquarium.tsx
+++ src/components/aquarium/aquarium.tsx
@@ -5,11 +5,7 @@ import { SquaresContainer } from '../squares-container/squares-container';
 import '../../_common.scss';
 import './aquarium.scss';
 
-// const initialRelief = [4, 2, 3, 2, 5, 0, 1, 3];
-// const initialRelief = [18,29,22,46,42,33,30,49,3,18,20,13,7,19,35,23,29,45,36,1,4,36,11,16,4,47,39,26,12,4,2,36,13,0,17,29,15,24,38,4,20,28,15,43,14,42,45,50,31,47,33,46,6,32,47,25,33,9,39,35,0,41,24,34];
-// const initialRelief = [18,29,22,29,12];
-// const initialRelief = [4,0,4,0,5,0,3];
-const initialRelief = [4,0,4,0,5,0,0,0,0,0,0,0,0,0,0,0,3];
+const initialRelief = [18,29,22,46,42,33,30,49,3,18,20,13,7,19,35,23,29,45,36,1,4,36,11,16,4,47,39,26,12,4,2,36,13,0,17,29,15,24,38,4,20,28,15,43,14,42,45,50,31,47,33,46,6,32,47,25,33,9,39,35,0,41,24,34];
 
 const aquariumStore = new AquariumStore({
   initialRelief,
